d466da810557685cf441ff11823b4b042c9edcd4
Anton Ilyin <ilyin.a.e@gmail.com>
2022-10-27 21:56:38 +0300

    Added code comments

--- code/Program.cs
+++ code/Program.cs
@@ -10,28 +10,30 @@ int FillInputArray(string[] inputArray, int minLength)
     for (int i = 0; i < inputArray.Count(); i++)
     {
         Console.WriteLine($"Введите {i + 1}-й элемент массива:");
-        string curValue = Console.ReadLine();
-        inputArray[i] = curValue;
-        if (curValue.Length < minLength)
+        string curValue = Console.ReadLine();           // считываем введенное значение из консоли
+        inputArray[i] = curValue;                       // записываем его во воходной массив
+        if (curValue.Length < minLength)                // если его длина меньше пороговой для "коротких строк" - считаем сколько введено таких элементов чтобы создать массив выходных значений
             outValuesCount++;
     }
-    return outValuesCount;
+    return outValuesCount;                              // возвращаем количество введенных коротких строк        
 }
+
 Console.Clear();
-Console.WriteLine("Сколько строк будем вводить?");
+Console.WriteLine("Сколько строк будем вводить?");                                                  // задаем длину входного массива
 int inputValuesCount = Int32.Parse(Console.ReadLine());
-Console.WriteLine("Сколько символов должно быть в строке чтобы она уже не считалась короткой?");
+Console.WriteLine("Сколько символов должно быть в строке чтобы она уже не считалась короткой?");    // задаем пороговое значение длины строки для занесения в результирующий массив
 int minLength = Int32.Parse(Console.ReadLine());
 string[] inputArray = new string[inputValuesCount];
-string[] outArray = new string[FillInputArray(inputArray, minLength)];
+string[] outArray = new string[FillInputArray(inputArray, minLength)];                              // создаем выходной массив длиной по числу введенных коротких строк
 int j = 0;
-for (int i = 0; i < inputValuesCount; i++)
+for (int i = 0; i < inputValuesCount; i++)                                                          // для каждого элемента входного массива - если он короче чем заданное пороговое значение 
 {
     if (inputArray[i].Length < minLength)
     {
-        outArray[j] = inputArray[i];
+        outArray[j] = inputArray[i];                                                                // записываем его в выходной массив
         j++;
     }
 }
-Console.WriteLine($"[{string.Join(", ", inputArray)}]");
+
+Console.WriteLine($"[{string.Join(", ", inputArray)}]");        //результирующий вывод массивов в консоль
 Console.WriteLine($"[{string.Join(", ", outArray)}]");
